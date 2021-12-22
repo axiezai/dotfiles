daeec16ffd5c88125c2e69b26dd56ddd6de04f86
Xihe Xie <axiezai@gmail.com>
2021-12-22 11:41:59 -0500

    better documenting steps

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
+# Brew
+echo "Installing homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
- echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/xihe/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/xihe/.zprofile
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
@@ -9,6 +11,10 @@ brew install git thefuck tmux reattach-to-user-namespace fzf rbenv ripgrep ruby
 $(brew --prefix)/opt/fzf/install
 
 # For agnoster zsh theme
+echo "Installing fonts for agnoster zsh theme"
 git clone https://github.com/powerline/fonts
 cd fonts
 ./install.sh
+cd ..
+rm -rf fonts
+echo "Deleting fonts repo after installation"
