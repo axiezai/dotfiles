fc42655626317e23201323bcf65ca7e8dc27a7a9
Xihe Xie <axiezai@gmail.com>
2021-12-22 11:41:38 -0500

    source macos

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # variables:
 dir=~/dotfiles # dot files directory
 olddir=~/dotfiles_old # backup dot files directory
-files="zshrc bash_aliases bash_profile tmux.conf vimrc vim fzf.bash fzf.zsh" # list of symlinks
+files="zshrc bash_aliases bash_profile tmux.conf vimrc vim fzf.bash fzf.zsh macos" # list of symlinks
 
 # create backup
 echo "Creating $olddir for backup of existing dotfiles in ~"
@@ -29,3 +29,7 @@ for file in $files; do
 		ln -s $dir/$file ~/.$file
 	fi
 done
+
+# MacOS Defaults:
+echo "Loading MacOS settings"
+source ~/.macos
