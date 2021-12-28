37c52eb959fa921c8506ccdb1994e8e194443b1d
Xihe Xie <axiezai@gmail.com>
2021-12-28 17:16:20 -0500

    use xihe

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo "Installing homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/xihe/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
