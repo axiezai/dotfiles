e1ae4324e6220929e3a482aa70bed38940976e6e
Xihe Xie <axiezai@gmail.com>
2021-12-28 19:28:23 -0500

    add tree

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 echo "Installing homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-brew install git thefuck tmux reattach-to-user-namespace fzf rbenv ripgrep ruby
+brew install git thefuck tmux reattach-to-user-namespace fzf rbenv ripgrep ruby tree
 
 $(brew --prefix)/opt/fzf/install
 
